fab0a69a9439cce56fa0bbf1c691cdf7b38f757e
genjiboy0312 <zxo9988@naver.com>
2023-06-15 17:33:51 +0900

    Push

--- Assets/GameCode/SparrowController.cs
+++ Assets/GameCode/SparrowController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class SparrowController : MonoBehaviour
 {
-    [SerializeField] private List<Transform> _wayPoints; // 이동 경로 웨이포인트
-    [SerializeField] private Transform _currentWaypoint; // 현재 웨이포인트
-    [SerializeField] private float _speed = 5f; // 이동 속도
-    [SerializeField] private bool _isMoving; // 이동 중인지 여부
+    [SerializeField] private List<Transform> _wayPoints;                    //  이동 경로 웨이포인트
+    private Transform _currentWaypoint;                    //  현재 웨이포인트
+    [SerializeField] private float _speed = 5f;                             //  이동 속도
+    [SerializeField] private bool _isMoving;                                //  이동 중인지 여부
+
+    [Header("Event Settings")]
+    [SerializeField] [Range(0f, 1f)] private float _spawnProbability = 0.3f; // 생성 확률
+    [SerializeField] private GameObject _pooPrefab;                         //  생성할 프리팹
+    [SerializeField] private bool _isInstantiate;                           //  true 누른 곳만 생성 되게 제한
 
     void Start()
     {
@@ -37,6 +42,17 @@ public class SparrowController : MonoBehaviour
             {
                 _currentWaypoint = _wayPoints[Random.Range(0, _wayPoints.Count)];
                 transform.LookAt(_currentWaypoint.position);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("WayPoint"))
+        {
+            if (_isInstantiate && Random.value <= _spawnProbability)
+            {
+                Instantiate(_pooPrefab, transform.position, transform.rotation);
             }
         }
     }
